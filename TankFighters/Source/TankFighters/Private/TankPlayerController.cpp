84a29f0ff80d7b6c6a7681431197bc7712c5328b
Valey-PC\Valey <Valey@Valey-PC.com>
2018-01-28 17:39:21 +0100

    LineTracefunzt

--- TankFighters/Source/TankFighters/Private/TankPlayerController.cpp
+++ TankFighters/Source/TankFighters/Private/TankPlayerController.cpp
@@ -2,9 +2,10 @@
 
 #include "TankPlayerController.h"
 #include "GameFramework/Actor.h"
-
-
-
+#include "Engine/GameViewportClient.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "Camera/PlayerCameraManager.h"
 
 void ATankPlayerController::BeginPlay()
 {
@@ -24,9 +25,57 @@ void ATankPlayerController::BeginPlay()
 void ATankPlayerController::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
-	UE_LOG(LogTemp, Warning, TEXT("Event Ticking"))
+	AimTowardsCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
+} 
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; //OutParam	
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *HitLocation.ToString())
+				//TODO tell controlled tank to aim there
+	}
 }
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	//Find Crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection)) 
+	{
+		GetLookVectorHitLocation(LookDirection, HitLocation);
+	}
+
+	//Line trace along that look direction, and see what we hit (up to max range)
+	return true;
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECollisionChannel::ECC_Visibility)) {
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	else { return false; }
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenPos, FVector& LookDirection) const {
+	FVector CameraWorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenPos.X, ScreenPos.Y, CameraWorldLocation, LookDirection);
+}
+
--- TankFighters/Source/TankFighters/Public/TankPlayerController.h
+++ TankFighters/Source/TankFighters/Public/TankPlayerController.h
@@ -7,6 +7,7 @@
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/Actor.h"
 #include "Public/Tank.h"
+#include "Engine/World.h"
 #include "TankPlayerController.generated.h"
 
 UCLASS()
@@ -14,11 +15,29 @@ class TANKFIGHTERS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-	
 public:
-	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
-
 	virtual void Tick( float DeltaTime ) override;
+
+private:
+
+	ATank * GetControlledTank() const;
+
+	//start moving the barrel so that a shot would hit where the crosshair points at the world
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333333333f;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 };
